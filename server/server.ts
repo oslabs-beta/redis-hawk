ca027d244775dfaf01523b907b012e69e546ebb8
Wesley Jia <wesley.jia94@gmail.com>
2021-05-14 20:19:41 -0400

    completed typescript compiler

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = +process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (req, res): void => {
   res.status(200).sendFile('../index.html');
 })
 
-module.exports = app.listen(PORT, () => {
+module.exports = app.listen(PORT, (): void => {
   console.log(`Listening on port ${PORT}`);
 });
