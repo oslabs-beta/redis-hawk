57ea51500772ee4826d1183b554d532a066bc99c
Wesley Jia <wesley.jia94@gmail.com>
2021-05-16 23:15:00 -0400

    Created the Redis monitoring interface with working event logs; added sample event route to server for testing

---
+++ server/redis-monitors/redis-monitors.ts
@@ -0,0 +1,62 @@
+/*
+Configures the RedisMonitors, which provide an interface for:
+* Working with a Redis client for a specific Redis instance
+* Reading monitored/logged data for Redis instances
+
+Each RedisMonitor will also create a keyspace notification subscriber
+for each keyspace and write them to an event log.
+*/
+
+const fs = require('fs');
+const path = require('path');
+const redis = require('redis');
+
+import { RedisInstance, RedisMonitor, Keyspace } from './models/interfaces';
+import { EventLog } from './models/data-stores';
+
+const instances: RedisInstance[] = JSON.parse(fs.readFileSync(path.resolve(__dirname, './config.json')));
+const redisMonitors: RedisMonitor[] = [];
+
+instances.forEach((instance: RedisInstance, idx: number): void => {
+
+  const monitor: RedisMonitor = {
+    redisClient: redis.createClient({host: instance.host, port: instance.port}),
+    host: instance.host,
+    port: instance.port,
+    keyspaces: []
+  }
+
+  monitor.databases = monitor.redisClient.config('GET', 'databases');
+  monitor.redisClient.config('GET', 'databases', (err, res: [string, string]): void => {
+
+    //Sets the number of databases present in this monitored Redis instance
+    monitor.databases = +res[1];
+    console.log(`Instance at ${monitor.host}:${monitor.port} has ${monitor.databases} databases`);
+    
+    //Configures each keyspace with a event subscriber and event log
+    //This should be futher modularized for readability and maintanability
+    for (let dbIndex = 0; dbIndex < monitor.databases; dbIndex++) {
+
+      const keyspace: Keyspace = {
+        eventLog: new EventLog(),
+        keySnapshots: []
+      }
+      monitor.keyspaces.push(keyspace);
+
+      //Sets up a listener to log any received events for this specific keyspace
+      monitor.redisClient.on('pmessage', (channel: string, message: string, event: string): void => {
+        if (+message.match(/[0-9*]/)[0] === dbIndex) {
+          const key = message.replace(/__keyspace@[0-9]*__/, '');
+          monitor.keyspaces[dbIndex].eventLog.add(key, event);
+          console.log(monitor.keyspaces[dbIndex].eventLog);
+        }
+      })
+    }
+
+    monitor.redisClient.psubscribe('__keyspace@*__:*')
+  });
+
+  redisMonitors.push(monitor);
+})
+
+module.exports = redisMonitors;
