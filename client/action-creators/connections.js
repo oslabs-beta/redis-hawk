e20846cdbaa85b6d6271770f4212e6d050f7b541
Arthur Sato <swatto12@gmail.com>
2021-05-18 14:06:05 -0400

    added action creator tests, action types, action creators

--- client/action-creators/connections.js
+++ client/action-creators/connections.js
@@ -1,40 +1,89 @@
-// export const deleteMediaActionCreator = (mediaId, userId) =>  (dispatch) => {  
-  
-//   fetch(`http://localhost:3000/api/media/${userId}/${mediaId}`, {
-//     method: 'DELETE',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     }})
-//   .then(res => res.json())
-//   .then(deletedDoc => { //the response be the deleted object, and we will grab the id off of that and we then go and fileter that out of state.
-   
-//     console.log('deletedDoc', deletedDoc);
-//     const deleteId = deletedDoc._id
-//     dispatch({
-//       type: actions.DELETE_MEDIA,
-//       payload: deleteId
-//     })
-//   .catch(err => {
-//     console.log('error deleting user from the DB in delteMedieActionCreator: ', err)
-//   })
-// });
+import * as types from "../actions/actionTypes";
 
+export const updateKeyspaceActionCreator = (instanceId, dbIndex) =>  (dispatch) => {  
+  
+  fetch(`http://localhost:3000/api/keyspace/${instanceId}/${dbIndex}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+  .then((res) => res.json())
+  .then((data) => {
+    //the response be the deleted object, and we will grab the id off of that and we then go and fileter that out of state.
 
-// export const addCardActionCreator = (marketId) => ({
-//     type: types.ADD_CARD,
-//     payload: marketId,
-//   });
-  
-//   export const removeCardActionCreator = (marketId) => ({
-//     type: types.DELETE_CARD,
-//     payload: marketId,
-//   });
-  
-//   export const addMarketActionCreator = () => ({
-//     type: types.ADD_MARKET,
-//   });
-  
-//   export const newLocationActionCreator = (location) => ({
-//     type: types.SET_NEW_LOCATION,
-//     payload: location,
-//   });
+    console.log("keyspace", data.keyspaces);
+    const keyspace = data.keyspaces;
+    dispatch({
+      type: actions.UPDATE_KEYSPACE,
+      //is this the proper syntax to grab dbIndex too????
+      payload: [keyspace, dbIndex]
+    }).catch((err) => {
+      console.log(
+        "error deleting user from the DB in keyspaceUpdateActionCreator: ",
+        err
+      );
+    });
+  });
+};
+
+export const updateEventsActionCreator =
+  (instanceId, dbIndex) => (dispatch) => {
+    
+    fetch(`http://localhost:3000/api/events/${instanceId}/${dbIndex}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      //the response be the deleted object, and we will grab the id off of that and we then go and fileter that out of state.
+
+      console.log("events", data.events);
+      const events = data.events;
+      dispatch({
+        type: actions.UPDATE_EVENTS,
+        //is this the proper syntax to add dbIndex???
+        payload: [events, dbIndex]
+      }).catch((err) => {
+        console.log(
+          "error deleting user from the DB in keyspaceUpdateActionCreator: ",
+          err
+        );
+      });
+    });
+  };
+
+  //NEEDS COMPLETION ONCE WE GET WHAT THE DATA WILL LOOK LIKE FROM BACKEND
+  export const updateKeyGraphActionCreator =
+    (instanceId, dbIndex) => (dispatch) => {
+      fetch(`http://localhost:3000/api/keyspaceHistory/${instanceId}/${dbIndex}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          //the response be the deleted object, and we will grab the id off of that and we then go and fileter that out of state.
+
+          console.log("events", data.keyspaceHistory);
+          const eventsHistory = data.keyspaceHistory;
+          dispatch({
+            type: actions.UPDATE_KEYGRAPH,
+            payload: events,
+          }).catch((err) => {
+            console.log(
+              "error deleting user from the DB in keyspaceUpdateActionCreator: ",
+              err
+            );
+          });
+        });
+    };
+
+    //SWITCH DATABASE
+    export const switchDatabaseActionCreator = (dbIndex) => ({
+      type: types.UPDATE_DATABASE,
+      payload: dbIndex
+    })
