b9fb27d1a208cfb0ca583a4139c573e789b5f303
Arthur Sato <swatto12@gmail.com>
2021-05-18 14:03:56 -0400

    adjusted tests for components and reducers for current version of state

--- __tests__/client/react/databaseNav.test.js
+++ __tests__/client/react/databaseNav.test.js
@@ -1,51 +1,52 @@
-import React from 'react';
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import toJson from 'enzyme-to-json';
-import renderer from 'react-test-renderer';
+import React from "react";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import toJson from "enzyme-to-json";
+import renderer from "react-test-renderer";
 
-import DatabaseNav from '../../../client/components/navbars/DatabaseNav.jsx';
-import DatabaseComponent from '../../../client/components/navbars/DatabaseComponent.jsx';
+import DatabaseNav from "../../../client/components/navbars/DatabaseNav.jsx";
+import DatabaseComponent from "../../../client/components/navbars/DatabaseComponent.jsx";
 
 configure({ adapter: new Adapter() });
 
-describe('React DatabaseNav unit tests', () => {
-  describe('DatabaseNav', () => {
+describe("React DatabaseNav unit tests", () => {
+  describe("DatabaseNav", () => {
     let wrapper;
     const props = {
       databaseInfo: [
         {
-          host: 'localhost',
-          port: '3000',
-          dataNum: 0,
+          host: "localhost",
+          port: "3000",
+          dataNum: 16,
         },
       ],
+      currDatabase: 0
     };
 
     beforeAll(() => {
       wrapper = shallow(<DatabaseNav props={props.databaseInfo} />);
     });
 
-    it('renders the div with id redisInstance with database host and database port passed down and a div with id databaseHolder with databaseInfo props passed down ', () => {
-      expect(wrapper.find('redisInstance'));
-      expect(wrapper.find('databaseHolder'));
+    it("renders the div with id redisInstance with database host and database port passed down and a div with id databaseHolder with databaseInfo props passed down ", () => {
+      expect(wrapper.find("redisInstance"));
+      expect(wrapper.find("databaseHolder"));
       expect(
         wrapper.containsAllMatchingElements([
           <div
             databaseInfo={[
               {
-                host: 'localhost',
-                port: '3000',
-                dataNum: 0,
+                host: "localhost",
+                port: "3000",
+                dataNum: 16,
               },
             ]}
           />,
           <div
             databaseInfo={[
               {
-                host: 'localhost',
-                port: '3000',
-                dataNum: 0,
+                host: "localhost",
+                port: "3000",
+                dataNum: 16,
               },
             ]}
           />,
@@ -53,15 +54,15 @@ describe('React DatabaseNav unit tests', () => {
       ).toEqual(true);
     });
   });
-  describe('DatabaseComponent', () => {
+  describe("DatabaseComponent", () => {
     let wrapper;
-    const changeDatabasesFunc = () => 'changing databases';
+    const changeDatabasesFunc = () => "changing databases";
     const props = {
       databaseInfo: [
         {
-          host: 'localhost',
-          port: '3000',
-          dataNum: 0,
+          host: "localhost",
+          port: "3000",
+          dataNum: 16,
         },
       ],
       changeDatabases: changeDatabasesFunc,
@@ -70,24 +71,24 @@ describe('React DatabaseNav unit tests', () => {
     beforeAll(() => {
       wrapper = shallow(<DatabaseComponent props={props.databaseInfo} />);
     });
-    it('renders a div with id singleDatabase that contains the number for the database', () => {
-      expect(wrapper.find('singleDatabase').find('div'));
+    it("renders a div with id singleDatabase that contains the number for the database", () => {
+      expect(wrapper.find("singleDatabase").find("div"));
       expect(
         wrapper.containsAllMatchingElements([
           <div
             databaseInfo={[
               {
-                host: 'localhost',
-                port: '3000',
-                dataNum: 0,
+                host: "localhost",
+                port: "3000",
+                dataNum: 16,
               },
             ]}
           />,
         ])
       );
     });
-    it('should have functions passed down invoking on click to change databases', () => {
-      expect(wrapper.find('singleDatabase').invoke('onClick')()).toEqual(
+    it("should have functions passed down invoking on click to change databases", () => {
+      expect(wrapper.find("singleDatabase").invoke("onClick")()).toEqual(
         changeDatabasesFunc()
       );
     });
