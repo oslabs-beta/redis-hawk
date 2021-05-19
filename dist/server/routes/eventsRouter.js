18333a182ab7453e6fcab8f64a22350fe1704053
James Espy <jamesespy@Jamess-MacBook-Air.local>
2021-05-19 18:15:50 -0500

    event routes separated, working on tests

--- dist/server/routes/eventsRouter.js
+++ dist/server/routes/eventsRouter.js
@@ -1,56 +1,52 @@
 var express = require('express');
 var eventsRouter = express.Router();
 var eventsMonitors = require('../redis-monitors/redis-monitors');
-eventsRouter.get('/:instanceId/:dbIndex', function (req, res, next) {
-    var _a = req.params, instanceId = _a.instanceId, dbIndex = _a.dbIndex;
+eventsRouter.get('/', function (req, res, next) {
     var body = { data: [] };
-    console.log('got in');
-    if (instanceId === undefined) {
-        eventsMonitors.forEach(function (monitor) {
-            var instance = {
-                instanceId: monitor.instanceId,
-                keyspaces: []
-            };
-            monitor.keyspaces.forEach(function (keyspace) {
-                var eventTotal = req.query.eventTotal;
-                instance.keyspaces.push(eventsMonitors.eventLog.returnLogAsArray(eventTotal ? eventTotal : 0));
-            });
-            body.data.push(instance);
-        });
-    }
-    ;
-    if (instanceId !== undefined && dbIndex === undefined) {
-        eventsMonitors[instanceId].forEach(function (monitor) {
-            var instance = {
-                instanceId: monitor.instanceId,
-                keyspaces: []
-            };
-            monitor.keyspaces.forEach(function (keyspace) {
-                instance.keyspaces.push(eventsMonitors.eventLog.returnLogAsArray(keyspace.eventLog));
-            });
-            body.data.push(instance);
-        });
-    }
-    if (dbIndex !== undefined) {
-        var monitor = eventsMonitors.find(function (m) {
-            console.log(instanceId);
-            console.log(m.instanceId);
-            return m.instanceId === +instanceId;
-        });
-        console.log(monitor);
+    eventsMonitors.forEach(function (monitor) {
         var instance = {
             instanceId: monitor.instanceId,
             keyspaces: []
         };
-        console.log("got into keyspaces loop");
-        var eventTotal = req.query.eventTotal;
-        console.log("eventTotal:  " + eventTotal);
-        instance.keyspaces.push(monitor.keyspaces[dbIndex].eventLog.returnLogAsArray((eventTotal) ? eventTotal : 0));
-        console.log("instance: " + instance);
-        console.log("got to body.data.push");
+        monitor.keyspaces.forEach(function (keyspace) {
+            var eventTotal = req.query.eventTotal;
+            instance.keyspaces.push(keyspace.eventLog.returnLogAsArray((eventTotal) ? eventTotal : 0));
+        });
         body.data.push(instance);
-    }
-    console.log('got to res statment');
+    });
+    res.status(200).json(body);
+});
+eventsRouter.get('/:instanceId/', function (req, res, next) {
+    var body = { data: [] };
+    var instanceId = req.params.instanceId;
+    var monitor = eventsMonitors.find(function (m) {
+        return m.instanceId === +instanceId;
+    });
+    var instance = {
+        instanceId: monitor.instanceId,
+        keyspaces: []
+    };
+    monitor.keyspaces.forEach(function (keyspace) {
+        var eventTotal = req.query.eventTotal;
+        instance.keyspaces.push(keyspace.eventLog.returnLogAsArray((eventTotal) ? eventTotal : 0));
+    });
+    console.log(instance);
+    body.data.push(instance);
+    res.status(200).json(body);
+});
+eventsRouter.get('/:instanceId/:dbIndex', function (req, res, next) {
+    var body = { data: [] };
+    var _a = req.params, instanceId = _a.instanceId, dbIndex = _a.dbIndex;
+    var monitor = eventsMonitors.find(function (m) {
+        return m.instanceId === +instanceId;
+    });
+    var instance = {
+        instanceId: monitor.instanceId,
+        keyspaces: []
+    };
+    var eventTotal = req.query.eventTotal;
+    instance.keyspaces.push(monitor.keyspaces[dbIndex].eventLog.returnLogAsArray((eventTotal) ? eventTotal : 0));
+    body.data.push(instance);
     res.status(200).json(body);
 });
 module.exports = eventsRouter;
--- server/routes/eventsRouter.ts
+++ server/routes/eventsRouter.ts
@@ -20,7 +20,7 @@ eventsRouter.get('/', (req, res, next) => {
       //for each keyspace, grab keyspace.eventLog, convert into array using returnLogAsArray and push into instance.keyspaces
       //check if req.query.eventTotal != undefined, if so use as argument for returnLogAsArray
       const { eventTotal } = req.query;
-      instance.keyspaces.push(eventsMonitors.eventLog.returnLogAsArray(eventTotal ? eventTotal : 0))
+      instance.keyspaces.push(keyspace.eventLog.returnLogAsArray((eventTotal) ? eventTotal : 0))
     })
     //push instance object into body.data
     body.data.push(instance)
@@ -33,20 +33,25 @@ eventsRouter.get('/:instanceId/', (req, res, next) => {
   //set const body = {data: []}
   const body = { data: [] };
   const { instanceId } = req.params
-  //if so, iterate through all monitors at that instanceId
-  eventsMonitors[instanceId].forEach(monitor => {
-    //initialize object instance as in previous section
-    const instance = {
-      instanceId: monitor.instanceId,
-      keyspaces: []
-    };
-    //loop through monitor.keyspaces array
-    monitor.keyspaces.forEach(keyspace => {
-      //grab keyspace.eventLog, convert into array and push into instance.keyspaces
-      instance.keyspaces.push(eventsMonitors.eventLog.returnLogAsArray(keyspace.eventLog))
-    });
-    body.data.push(instance);
-  })
+  //find monitor based on instanceId
+  const monitor = eventsMonitors.find(m => {
+    return m.instanceId === +instanceId;
+  });
+  //initialize object instance as in previous section
+  const instance = {
+    instanceId: monitor.instanceId,
+    keyspaces: []
+  };
+  //loop through monitor.keyspaces array
+  monitor.keyspaces.forEach(keyspace => {
+    //grab keyspace.eventLog, convert into array and push into instance.keyspaces
+    const { eventTotal } = req.query;
+    instance.keyspaces.push(keyspace.eventLog.returnLogAsArray((eventTotal) ? eventTotal : 0))
+  });
+
+
+  console.log(instance)
+  body.data.push(instance);
   //res.send jsonified body.data
   res.status(200).json(body);
 });
@@ -66,7 +71,6 @@ eventsRouter.get('/:instanceId/:dbIndex', (req, res, next) => {
   };
   //grab keyspace.eventLog, convert into array and push into instance.keyspaces
   const { eventTotal } = req.query;
-  console.log("eventTotal:  " + eventTotal)
   instance.keyspaces.push(monitor.keyspaces[dbIndex].eventLog.returnLogAsArray((eventTotal) ? eventTotal : 0))
   body.data.push(instance);
   //res.send jsonified body.data
