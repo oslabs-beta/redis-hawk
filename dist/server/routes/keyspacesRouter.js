98564c64d8508ebf4548bd623259e21672994916
Wesley Jia <wesley.jia94@gmail.com>
2021-05-20 00:43:00 -0400

    /api/keyspaces handling route parameters

--- dist/server/routes/keyspacesRouter.js
+++ dist/server/routes/keyspacesRouter.js
@@ -3,7 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var keyspacesController = require('../controllers/keyspacesController');
 var keyspacesRouter = express_1.Router();
-keyspacesRouter.get('/', keyspacesController.getKeyspace, function (req, res) {
+keyspacesRouter.get('/', keyspacesController.getAllInstancesKeyspaces, function (req, res) {
+    res.status(200).json({ data: res.locals.data });
+});
+keyspacesRouter.get('/:instanceId', keyspacesController.getAllKeyspacesForInstance, function (req, res) {
+    res.status(200).json({ data: res.locals.data });
+});
+keyspacesRouter.get('/:instanceId/:dbIndex', keyspacesController.getKeyspaceForInstance, function (req, res) {
     res.status(200).json({ data: res.locals.data });
 });
 module.exports = keyspacesRouter;
--- server/controllers/keyspacesController.ts
+++ server/controllers/keyspacesController.ts
@@ -6,15 +6,19 @@ import { getKeyspace } from './utils';
 import { createClient } from 'redis';
 
 interface keyspacesController {
-  getKeyspace?: (req, res, next) => void;
+  getAllInstancesKeyspaces?: (req, res, next) => void;
+  getAllKeyspacesForInstance?: (req, res, next) => void;
+  getKeyspaceForInstance?: (req, res, next) => void;
 }
 
 const keyspacesController: keyspacesController = {};
 
-keyspacesController.getKeyspace = async (req, res, next) => {
+keyspacesController.getAllInstancesKeyspaces = async (req, res, next) => {
+/*
+Retrieves all keyspace data for every keyspace of every monitored instance.
+*/
 
   const data: RedisInstance[] = [];
-
   for (let monitor of keyspacesRedisMonitors) {
     
     const keyspaces: Keyspace[] = [];
@@ -32,12 +36,60 @@ keyspacesController.getKeyspace = async (req, res, next) => {
       keyspaces: keyspaces
     });
   }
-
   res.locals.data = data;
-
   return next();
 }
 
+keyspacesController.getAllKeyspacesForInstance = async (req, res, next) => {
 
+  const data: RedisInstance[] = [];
+  for (let monitor of keyspacesRedisMonitors) {
+
+    if (monitor.instanceId = req.params.instanceId) {
+    
+      const keyspaces: Keyspace[] = [];
+      let idx = 0;
+
+      const client = createClient({host: monitor.host, port: monitor.port});
+      for (const keyspace of monitor.keyspaces) {
+        const keyspaceData = await getKeyspace(client, idx)
+        keyspaces.push(keyspaceData);
+        idx += 1;
+      }
+
+      data.push({
+        instanceId: monitor.instanceId,
+        keyspaces: keyspaces
+      });
+    }
+  }
+  res.locals.data = data;
+  return next();
+}
+
+keyspacesController.getKeyspaceForInstance = async (req, res, next) => {
+
+  const data: RedisInstance[] = [];
+  for (let monitor of keyspacesRedisMonitors) {
+
+    if (monitor.instanceId = req.params.instanceId) {
+    
+      const keyspaces: Keyspace[] = [];
+      let idx = 0;
+
+      const client = createClient({host: monitor.host, port: monitor.port});
+      const keyspaceData = await getKeyspace(client, req.params.dbIndex);
+      keyspaces.push(keyspaceData);
+
+      data.push({
+        instanceId: monitor.instanceId,
+        keyspaces: keyspaces
+      });
+
+    }
+  }
+  res.locals.data = data;
+  return next();
+}
 
 module.exports = keyspacesController;
