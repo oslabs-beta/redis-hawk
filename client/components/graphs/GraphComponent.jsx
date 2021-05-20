3a14f30fe7c698c0d1a5af0a71bbe6ae317b2930
Abigail Gjurich <amgjurich@gmail.com>
2021-05-20 13:02:55 -0400

    got graph component to fetch on a set interval rather than having to click refresh

--- client/components/graphs/GraphComponent.jsx
+++ client/components/graphs/GraphComponent.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
-import GraphHolder from "./GraphHolder.jsx";
-import { connect } from "react-redux";
-import * as actions from "../../action-creators/connections";
+import React, { Component } from 'react';
+import GraphHolder from './GraphHolder.jsx';
+import { connect } from 'react-redux';
+import * as actions from '../../action-creators/connections';
 
 const mapStateToProps = (store) => {
   return {
@@ -13,15 +13,30 @@ const mapStateToProps = (store) => {
 const mapDispatchToProps = (dispatch) => ({
   updateCurrentDisplay: (filter, category) =>
     dispatch(actions.updateCurrDisplayActionCreator(filter, category)),
+  updateEvents: (instanceId, dbIndex, currIndex) =>
+    dispatch(actions.updateEventsActionCreator(instanceId, dbIndex, currIndex)),
 });
 
 class GraphComponent extends Component {
   constructor(props) {
     super(props);
+    this.setGraphUpdate = this.setGraphUpdate.bind(this);
+  }
+
+  componentDidMount() {
+    const self = this;
+    console.log('props', this.props);
+    // setInterval(this.setGraphUpdate, 10000);
+    setInterval(self.setGraphUpdate, 3000);
+  }
+  setGraphUpdate() {
+    console.log('fetching');
+    let currIndex = this.props.events[this.props.currDatabase].length - 1;
+    this.props.updateEvents(1, this.props.currDatabase, currIndex);
   }
 
   render() {
-    console.log("props in GraphComponent", this.props);
+    console.log('props in GraphComponent', this.props);
     return (
       <div id='graphsComponentContainer'>
         <GraphHolder
