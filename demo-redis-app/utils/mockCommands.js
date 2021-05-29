5e5d34867b9a0133fb3391451db79453281d19ba
Wesley Jia <wesley.jia94@gmail.com>
2021-05-29 18:21:10 -0400

    disabling all hash commands as they break the app

--- demo-redis-app/utils/mockCommands.js
+++ demo-redis-app/utils/mockCommands.js
@@ -133,25 +133,25 @@ mockCommands.zpopminSortedSet = (client) => {
 }
 
 /* <<<<< Hashes >>>>> */
-mockCommands.hmsetHash = (client) => {
-  const key = mockData.hashes.createKey();
-  client.hmset(key, mockData.hashes.createValue(), (err, res) => {
-    if (!err) console.log(`Key hmset ${key}`);
-    //Randomly expire every other successfully generated key
-    if (!err & Math.floor(Math.random() * 2) % 2 === 0) {
-      client.expire(key, mockSettings.HASH_TIME_TO_LIVE, (err, res) => {
-        if (!err) console.log(`Hash key expried: ${key}`);
-      });
-    }
-  });
-}
+// mockCommands.hmsetHash = (client) => {
+//   const key = mockData.hashes.createKey();
+//   client.hmset(key, mockData.hashes.createValue(), (err, res) => {
+//     if (!err) console.log(`Key hmset ${key}`);
+//     //Randomly expire every other successfully generated key
+//     if (!err & Math.floor(Math.random() * 2) % 2 === 0) {
+//       client.expire(key, mockSettings.HASH_TIME_TO_LIVE, (err, res) => {
+//         if (!err) console.log(`Hash key expried: ${key}`);
+//       });
+//     }
+//   });
+// }
 
-mockCommands.hgetallHash = (client) => {
-  const key = mockData.hashes.createKey();
-  client.hgetall(key, (err, res) => {
-    if (!err) console.log(`Retrieved hash data for key ${key}:  ${res}`)
-  });
-}
+// mockCommands.hgetallHash = (client) => {
+//   const key = mockData.hashes.createKey();
+//   client.hgetall(key, (err, res) => {
+//     if (!err) console.log(`Retrieved hash data for key ${key}:  ${res}`)
+//   });
+// }
 
 // mockCommands.hdelHash = (client) => {
 //   const key = mockData.hashes.createKey();
