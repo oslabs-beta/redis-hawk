bc78b79706ef9e4504970b9bbf24ddb8201174db
Arthur Sato <swatto12@gmail.com>
2021-05-31 11:16:52 -0400

    added action creator and type for query to endpoint events/totals

--- client/action-creators/eventsConnections.js
+++ client/action-creators/eventsConnections.js
@@ -1,29 +1,33 @@
 import * as types from "../actions/actionTypes";
 
-//called on initial load of app in App.jsx
-// no requirements in for the deployment of this action creator
-//response :
-// {
-//   data: [ (array of instances)
-//     {
-//       instanceId: 1,
-//       keyspaces: [ (array of databases)
-//         {
-//           keyTotal: 6347,
-//           pageSize: 50,
-//           pageNum: 4,
-//           data: [
-//             {
-//               key: '',
-//               value: any,
-//               type: any,
-//             },
-//           ],
-//         },
-//       ],
-//     },
-//   ];
+// sample response from get to '/'
+//{
+//  data: [ (array of Instances)
+//   {
+//    instanceId: number,
+//    keyspaces: [ (array of Keyspaces)
+//      {
+//       eventTotal: 6347 (number),
+//       pageSize: 50 (number),
+//        pageNum: 4 (number),
+//        data: [ (Array of Key Details)
+//          {
+//          key: string,
+//          event: string,
+//          timestamp: Date
+//         }
+//        ]
+//       },
+//      {eventTotal, pageSize, pageNum, data: [etc]}
+//    ]
+//   },
+//   {
+//     instanceId: number,
+//     keyspaces: [..etc]
+//    }
+//  ]
 // }
+
 export const loadAllEventsActionCreator = () => (dispatch) => {
   fetch("/api/v2/events")
     .then((res) => res.json())
@@ -39,28 +43,22 @@ export const loadAllEventsActionCreator = () => (dispatch) => {
     });
 };
 
-//for refreshing the keyspace of a certain database at a certain instance - need to know if there are filters or not for dispatch
-// arguments: database, instance, page size, page num = 1, refreshScan = 1,
-//response:
-// {
-//     keyTotal: 6347,
-//     pageSize: 50,
-//     pageNum: 4,
-//     data: [
-//         {
-//             key: '',
-//             value: '',
-//             type: any,
-//         }
-//     ]
-// }
+// response for refresh: Object containing an event log subset, based on the page parameters and any filters. Event details are stored in the data property as an array of event objects, where each event object has the properties:
+
+// * key: name of the key
+
+// * event: type of the event
+
+// * timestamp: time when the event occurred
+
 export const refreshKeyspaceActionCreator =
   (instanceId, dbIndex, pageSize, pageNum, refreshData) => (dispatch) => {
     fetch(
-      `api/v2/keyspaces/${instanceId}/${dbIndex}/?pageSize=${pageSize}&pageNum=${pageNum}&refreshScan=${refreshData}`
+      `api/v2/events/${instanceId}/${dbIndex}/?pageSize=${pageSize}&pageNum=${pageNum}&refreshScan=${refreshData}`
     )
       .then((res) => res.json())
       .then((response) => {
+        // response should be 
         console.log("response in refreshEventsActionCreator", response);
         let refreshEvents = response;
         dispatch({
@@ -89,30 +87,62 @@ export const refreshKeyspaceActionCreator =
 //         }
 //     ]
 // }
-export const changeKeyspacePageActionCreator =
-  (instanceId, dbIndex, queryOptions) => (dispatch) => {
-    let URI = `api/v2/keyspaces/${instanceId}/${dbIndex}/?`;
-    //this may have an issue in here - be aware of queryOptions
-    if (queryOptions.pageSize) URI += `pageSize=${queryOptions.pageSize}`;
-    if (queryOptions.pageNum) URI += `&pageNum=${queryOptions.pageNum}`;
-    if (queryOptions.keyNameFilter)
-      URI += `&keyNameFilter=${queryOptions.keyNameFilter}`;
-    if (queryOptions.keyTypeFilter)
-      URI += `&keyTypeFilter=${queryOptions.keyTypeFilter}`;
-    if (queryOptions.refreshScan)
-      URI += `&refreshScan=${queryOptions.refreshScan}`;
+export const changeEventsPageActionCreator =
+  (instanceId, dbIndex, queryParams) => (dispatch) => {
+    let URI = `api/v2/events/${instanceId}/${dbIndex}/?`;
+    //this may have an issue in here - be aware of queryParams
+    if (queryParams.pageSize) URI += `pageSize=${queryParams.pageSize}`;
+    if (queryParams.pageNum) URI += `&pageNum=${queryParams.pageNum}`;
+    if (queryParams.keyNameFilters)
+      URI += `&keyNameFilter=${queryParams.keyNameFilters}`;
+    if (queryParams.keyTypeFilter)
+      URI += `&keyTypeFilter=${queryParams.keyTypeFilter}`;
+    if (queryParams.refreshData)
+      URI += `&refreshData=${queryParams.refreshData}`;
 
     fetch(URI)
       .then((res) => res.json)
       .then((response) => {
-        console.log("response in changeKeyspaceActionCreator", response);
-        let nextPageKeyspace = response;
+        console.log("response in changeEventsPageActionCreator", response);
+        let nextPageEvents = response;
         dispatch({
-          type: types.CHANGE_KEYSPACE_PAGE,
+          type: types.CHANGE_EVENTS_PAGE,
           payload: {
-            keyspace: nextPageKeyspace,
+            events: nextPageEvents,
             currInstance: instanceId,
             currDatabase: dbIndex,
+          },
+        });
+      });
+  };
+
+  export const getEventTotalsActionCreator = (instanceId, dbIndex, queryParams) => (dispatch) => {
+    let URI = `api/v2/events/${instanceId}/${dbIndex}/?`;
+    if (queryParams.eventTotal){
+      if (queryParams.eventTypes) URI += `eventTotal=${queryParams.eventTotal}/&${queryParams.eventTypes}`
+      if (queryParams.keynameFilters) URI += `eventTotal=${queryParams.eventTotal}/&keynameFilters=${queryParams.keynameFilters}`
+      else {
+        URI += `eventTotal=${queryParms.eventTotal}`;
+      }
+    }
+     if (queryParams.timeInterval) {
+       if (queryParams.eventTypes)
+         URI += `timeInterval=${queryParams.timeInterval}/&${queryParams.eventTypes}`;
+       if (queryParams.keynameFilters)
+         URI += `timeInterval=${queryParams.timeInterval}/&keynameFilters=${queryParams.keynameFilters}`;
+       else {
+         URI += `timeInterval=${queryParms.timeInterval}`;
+       }
+     }
+    fetch(URI)
+      .then((res) => res.json())
+      .then((response) => {
+        console.log("response in getEventTotalsActionCreator", response);
+        let allEvents = response.data;
+        dispatch({
+          type: types.LOAD_ALL_EVENTS,
+          payload: {
+            totalEvents: allEvents,
           },
         });
       });
