9f0b11703596d5369b588ee6fb4a9cbc70fedae5
Arthur Sato <swatto12@gmail.com>
2021-05-31 18:54:13 -0400

    added loadAllEvents to componentDidMount in App.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import FilterNav from './navbars/FilterNav.jsx';
-import PageNav from './navbars/PageNav.jsx';
-import InstanceNav from './navbars/InstanceNav.jsx';
-import DatabaseNav from './navbars/DatabaseNav.jsx';
-import KeyspaceComponent from './keyspace/KeyspaceComponent.jsx';
-import GraphComponent from './graphs/GraphComponent.jsx';
-import EventComponent from './events/EventComponent.jsx';
-import './styles/styles.scss';
-import { connect } from 'react-redux';
-import * as actions from '../action-creators/connections';
-import * as keyspaceActions from '../action-creators/keyspaceConnections';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import FilterNav from "./navbars/FilterNav.jsx";
+import PageNav from "./navbars/PageNav.jsx";
+import InstanceNav from "./navbars/InstanceNav.jsx";
+import DatabaseNav from "./navbars/DatabaseNav.jsx";
+import KeyspaceComponent from "./keyspace/KeyspaceComponent.jsx";
+import GraphComponent from "./graphs/GraphComponent.jsx";
+import EventComponent from "./events/EventComponent.jsx";
+import "./styles/styles.scss";
+import { connect } from "react-redux";
+import * as actions from "../action-creators/connections";
+import * as keyspaceActions from "../action-creators/keyspaceConnections";
 import * as eventActions from "../action-creators/eventsConnections";
-import '../../node_modules/react-vis/dist/style.css';
+import "../../node_modules/react-vis/dist/style.css";
 
 ///still need to check dispatchers here
 
@@ -25,8 +25,8 @@ const mapStateToProps = (store) => {
 
 const mapDispatchToProps = (dispatch) => ({
   loadKeyspace: () => dispatch(keyspaceActions.loadKeyspaceActionCreator()),
-  updateEvents: (dbIndex) =>
-    dispatch(actions.updateEventsActionCreator(dbIndex)),
+  loadEvents: (dbIndex) =>
+    dispatch(actions.loadAllEventsActionsCreator(dbIndex)),
   updateInstanceInfo: () => dispatch(actions.updateInstanceInfoActionCreator()),
 });
 
@@ -37,8 +37,7 @@ class App extends Component {
 
   componentDidMount() {
     this.props.loadKeyspace();
-    console.log('loaded keyspace');
-    // this.props.updateEvents();
+    this.props.loadAllEvents();
     this.props.updateInstanceInfo();
   }
 
