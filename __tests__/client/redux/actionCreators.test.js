cabdcb55b3b258ddba612037d17679ca3c904806
Arthur Sato <swatto12@gmail.com>
2021-05-31 00:17:03 -0400

    added test for changeKeyspacePageActionCreator

--- __tests__/client/redux/actionCreators.test.js
+++ __tests__/client/redux/actionCreators.test.js
@@ -1,28 +1,32 @@
-import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
-import { loadKeyspaceActionCreator, refreshKeyspaceActionCreator} from '../../../client/action-creators/connections';
-import * as actions from '../../../client/actions/actionTypes';
-import fetchMock from 'fetch-mock';
-import expect from 'expect'; // You can use any testing library
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import {
+  loadKeyspaceActionCreator,
+  refreshKeyspaceActionCreator,
+  changeKeyspacePageActionCreator,
+} from "../../../client/action-creators/keyspaceConnections";
+import * as actions from "../../../client/actions/actionTypes";
+import fetchMock from "fetch-mock";
+import expect from "expect"; // You can use any testing library
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
-describe('loadKeyspaceActionCreator', () => {
+describe("loadKeyspaceActionCreator", () => {
   afterEach(() => {
     fetchMock.restore();
   });
 
-  it('creates UPDATE_KEYSPACE when fetching keyspace has been done', () => {
-    fetchMock.getOnce('/api/v2/keyspaces', {
-      headers: { 'content-type': 'application/json' },
+  it("creates UPDATE_KEYSPACE when fetching keyspace has been done", () => {
+    fetchMock.getOnce("/api/v2/keyspaces", {
+      headers: { "content-type": "application/json" },
     });
 
     const expectedActions = [
       {
         type: actions.LOAD_KEYSPACE,
         payload: {
-          keyspace: [{ key: 'abigail', value: 'yes', type: 'string' }],
+          keyspace: [{ key: "abigail", value: "yes", type: "string" }],
         },
       },
     ];
@@ -31,7 +35,7 @@ describe('loadKeyspaceActionCreator', () => {
     return (
       store
         // .dispatch(connections.keyspaceUpdateActionCreator())
-        .dispatch(loadKeyspaceActionCreator(0, 1))
+        .dispatch(loadKeyspaceActionCreator())
         .then(() => {
           // return of async actions
           expect(store.getActions()).toEqual(expectedActions);
@@ -39,16 +43,21 @@ describe('loadKeyspaceActionCreator', () => {
     );
   });
 });
+
 describe("refreshKeyspaceActionCreator", () => {
   afterEach(() => {
     fetchMock.restore();
   });
 
   it("creates REFRESH_KEYSPACE when fetching keyspace has been done", () => {
+    const instanceId = 1;
+    const dbIndex = 2;
     const pageNum = 50;
     const pageSize = 10;
-    const refreshScan = false
-    fetchMock.getOnce(`/api/v2/keyspaces/2/?pageSize=${pageSize}&pageNum=${pageNum}&refreshScan=${refreshScan}`);
+    const refreshScan = 1;
+    fetchMock.getOnce(
+      `/api/v2/keyspaces/${instanceId}/${dbIndex}/?pageSize=${pageSize}&pageNum=${pageNum}&refreshScan=${refreshScan}`
+    );
 
     const expectedActions = [
       {
@@ -56,7 +65,7 @@ describe("refreshKeyspaceActionCreator", () => {
         payload: {
           keyspace: [{ key: "abigail", value: "yes", type: "string" }],
           currInstance: 2,
-          currDatabase: 1
+          currDatabase: 1,
         },
       },
     ];
@@ -65,7 +74,7 @@ describe("refreshKeyspaceActionCreator", () => {
     return (
       store
         // .dispatch(connections.keyspaceUpdateActionCreator())
-        .dispatch(refreshKeyspaceActionCreator(0, 1))
+        .dispatch(refreshKeyspaceActionCreator(0, 1, 50, 10, 1))
         .then(() => {
           // return of async actions
           expect(store.getActions()).toEqual(expectedActions);
@@ -74,14 +83,49 @@ describe("refreshKeyspaceActionCreator", () => {
   });
 });
 
-describe('updateEventActionCreator', () => {
+describe("changeKeyspacePageActionCreator", () => {
   afterEach(() => {
     fetchMock.restore();
   });
 
-  it('creates UPDATE_EVENTS when fetching is done', () => {
-    fetchMock.getOnce('/api/events/0/1', {
-      headers: { 'content-type': 'application/json' },
+  it("creates CHANGE_KEYSPACE_PAGE when fetching keyspace has been done", () => {
+    const instanceId = 1;
+    const dbIndex = 2;
+    fetchMock.getOnce(`/api/v2/keyspaces/${instanceId}/${dbIndex}?`);
+
+    const expectedActions = [
+      {
+        type: actions.CHANGE_KEYSPACE_PAGE,
+        payload: {
+          keyspace: [{ key: "abigail", value: "yes", type: "string" }],
+          currInstance: 1,
+          currDatabase: 2,
+        },
+      },
+    ];
+    const store = mockStore({ keyspace: [[]] });
+
+    return (
+      store
+        // .dispatch(connections.keyspaceUpdateActionCreator())
+        .dispatch(changeKeyspacePageActionCreator(1, 2))
+        .then(() => {
+          // return of async actions
+          expect(store.getActions()).toEqual(expectedActions);
+        })
+    );
+  });
+  // NEED TO ADD FILTER TESTS
+});
+
+describe("updateEventActionCreator", () => {
+  afterEach(() => {
+    fetchMock.restore();
+  });
+
+  it("creates UPDATE_EVENTS when fetching is done", () => {
+    fetchMock.getOnce("/api/events/0/1", {
+      headers: { "content-type": "application/json" },
     });
 
     const expectedActions = [{ type: types.UPDATE_EVENTS }];
@@ -95,14 +139,14 @@ describe('updateEventActionCreator', () => {
 });
 
 //NEEDS TO TRANSITION TO KEYGRAPHACTIONCREATOR - ONCE WE GET THE LOOK AT DATA
-describe('updateEventGraphActionCreator', () => {
+describe("updateEventGraphActionCreator", () => {
   afterEach(() => {
     fetchMock.restore();
   });
 
-  it('creates UPDATE_KEYGRAPH when fetching is done', () => {
-    fetchMock.getOnce('/api/graphs/0/1/events', {
-      headers: { 'content-type': 'application/json' },
+  it("creates UPDATE_KEYGRAPH when fetching is done", () => {
+    fetchMock.getOnce("/api/graphs/0/1/events", {
+      headers: { "content-type": "application/json" },
     });
 
     const expectedActions = [{ type: types.UPDATE_KEYGRAPH }];
@@ -115,18 +159,18 @@ describe('updateEventGraphActionCreator', () => {
   });
 });
 
-describe('updateDBInfoActionCreator', () => {
+describe("updateDBInfoActionCreator", () => {
   afterEach(() => {
     fetchMock.restore();
   });
 
-  it('dispatches an event to update the current DB', () => {
-    fetchMock.getOnce('/api/connections');
+  it("dispatches an event to update the current DB", () => {
+    fetchMock.getOnce("/api/connections");
 
     const expectedActions = [{ type: types.UPDATE_DBINFO }];
     const store = mockStore({
       databaseInfo: {
-        host: '',
+        host: "",
         port: 0,
         numberOfDBs: 0,
       },
@@ -141,50 +185,50 @@ describe('updateDBInfoActionCreator', () => {
 
 //SWITCH DATABASE
 
-describe('switchDatabaseActionCreator', () => {
+describe("switchDatabaseActionCreator", () => {
   afterEach(() => {
     fetchMock.restore();
   });
 
-  it('creates SWITCH_DATABASE when fetching is done', () => {
+  it("creates SWITCH_DATABASE when fetching is done", () => {
     const expectedActions = [{ type: types.UPDATE_DATABASE }];
     const store = mockStore({ currDatabase: 0 });
     expect(store.getActions().toEqual(expectedActions));
   });
 });
 
-describe('updatePageActionCreator', () => {
+describe("updatePageActionCreator", () => {
   afterEach(() => {
     fetchMock.restore();
   });
 
-  it('updates the current page', () => {
+  it("updates the current page", () => {
     const expectedActions = [{ type: types.UPDATE_PAGE }];
-    const store = mockStore({ newPage: '' });
+    const store = mockStore({ newPage: "" });
     expect(store.getActions().toEqual(expectedActions));
   });
 });
 
-describe('updateCurrDisplayActionCreator', () => {
+describe("updateCurrDisplayActionCreator", () => {
   afterEach(() => {
     fetchMock.restore();
   });
 
-  it('updates the current display', () => {
+  it("updates the current display", () => {
     const expectedActions = [{ type: types.UPDATE_CURRDISPLAY }];
-    const store = mockStore({ filter: '', category: '' });
+    const store = mockStore({ filter: "", category: "" });
     expect(store.getActions().toEqual(expectedActions));
   });
 });
 
 //SWITCH INSTANCE
 
-describe('switchInstanceActionCreator', () => {
+describe("switchInstanceActionCreator", () => {
   afterEach(() => {
     fetchMock.restore();
   });
 
-  it('creates SWITCH_INSTANCE when fetching is done', () => {
+  it("creates SWITCH_INSTANCE when fetching is done", () => {
     const expectedActions = [{ type: types.SWITCH_INSTANCE }];
     const store = mockStore({ currInstance: 1 });
     expect(store.getActions().toEqual(expectedActions));
