881afb3b1ca8db5653b0f44b6a3956e4d6b9e72f
Wesley Jia <wesley.jia94@gmail.com>
2021-05-31 22:14:11 -0400

    Consolidated the two instance/db components into one; DB selection is now a drop down menu

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import FilterNav from "./navbars/FilterNav.jsx";
 import PageNav from "./navbars/PageNav.jsx";
 import InstanceNav from "./navbars/InstanceNav.jsx";
-import DatabaseNav from "./navbars/DatabaseNav.jsx";
 import KeyspaceComponent from "./keyspace/KeyspaceComponent.jsx";
 import GraphComponent from "./graphs/GraphComponent.jsx";
 import EventComponent from "./events/EventComponent.jsx";
@@ -46,7 +45,6 @@ class App extends Component {
           <FilterNav />
           <PageNav />
           <InstanceNav />
-          <DatabaseNav />
           {/* create a react router to switch between the main area of divs */}
           <Switch>
             <Route exact path='/' render={() => <KeyspaceComponent />} />
--- client/components/navbars/InstanceNav.jsx
+++ client/components/navbars/InstanceNav.jsx
@@ -1,54 +1,65 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import InstanceComponent from "./InstanceComponent.jsx";
+import DatabaseSelector from "./DatabaseSelector.jsx";
 import * as actions from "../../action-creators/connections";
 
 const mapStateToProps = (store) => ({
   instanceInfo: store.instanceInfoStore.instanceInfo,
   currInstance: store.currInstanceStore.currInstance,
+  currDatabase: store.currDatabaseStore.currDatabase
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  switchInstance: (instance) =>
-    dispatch(actions.switchInstanceActionCreator(instance)),
+  switchInstance: (instance) => {
+    dispatch(actions.switchInstanceActionCreator(instance));
+
+    //When switching instances, automatically flip to db0
+    dispatch(actions.switchDatabaseActionCreator(0)); 
+  },
+  switchDatabase: (database) => {
+    dispatch(actions.switchDatabaseActionCreator(database));
+  }
 });
 
 class InstanceNav extends Component {
+
   constructor(props) {
     super(props);
   }
-  render() {
-    // if (this.props.instanceInfo[this.props.currInstance - 1]) {
 
-    const instanceArray = [];
-    if (this.props.currInstance) {
-      for (let i = 0; i < this.props.instanceInfo.length; i++) {
-        instanceArray.push(
-          <InstanceComponent
-            handleClick={this.props.switchInstance}
-            currInstance={i + 1}
-            key={i}
+  render() {
+
+    const instances = [];
+    this.props.instanceInfo.forEach((instance, idx) => {
+
+      //If instance is currently selected
+      //Pass down a DatabaseSelector component
+      let databases;
+      let instanceClickHandler;
+      if (instance.instanceId === this.props.currInstance) {
+        databases = <DatabaseSelector 
+          switchDatabase={this.props.switchDatabase}
+          dbCount={instance.databases}
+          currDatabase={this.props.currDatabase}
           />
-        );
       }
-    }
+
+      instances.push(
+        <InstanceComponent
+          instanceDetails={instance}
+          databases={databases}
+          switchInstance={this.props.switchInstance}
+          key={`instance-${idx}`}
+        />
+      )
+    })
+
     return (
       <div className='InstanceNav-Container'>
-        <div id='redisInstance' instanceinfo={this.props.currInstance}>
-          <div id='instanceHolder'>{instanceArray}</div>
-        </div>
+        {instances}
       </div>
     );
-
-    // else{
-    //   return (
-    //     <div className='InstanceNav-Container'>
-    //     <div id='redisInstance' instanceinfo={this.props.currInstance}>
-    //       <div id='instanceHolder'>loading</div>
-    //     </div>
-    //   </div>
-    //   )
-    // }
   }
 }
 
